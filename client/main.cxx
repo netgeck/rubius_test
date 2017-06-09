d0af4f3b7ed8ecfd15b51fb91a5dfda618f6178e
Konstantin Scornyakov <netgeck@gmail.com>
2017-06-09 21:21:26 +0700

    Созданы заготовки клиента и сервера

---
+++ client/main.cxx
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cxx
+ * Author: kostya
+ *
+ * Created on 9 июня 2017 г., 21:19
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+	return 0;
+}
+
