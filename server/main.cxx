0437c09289bf63e99c3ff503b7b788fee978dc04
Konstantin Scornyakov <netgeck@gmail.com>
2017-06-13 00:34:07 +0700

    server: рефакторинг; соединение вынесено в отдельный класс;

--- server/main.cxx
+++ server/main.cxx
@@ -21,10 +21,9 @@ using namespace std;
 
 namespace basio = boost::asio;
 using namespace boost::asio;
-typedef boost::shared_ptr<ip::tcp::socket> socket_ptr;
+typedef std::shared_ptr<ip::tcp::socket> socket_ptr;
 typedef std::shared_ptr<ip::tcp::acceptor> acceptor_ptr;
-acceptor_ptr gAcc;
-socket_ptr sock;
+socket_ptr gSock;
 
 void signalHandler(int signum) {
 	cout << "Interrupt signal (" << signum << ") received.\n";
@@ -55,25 +54,48 @@ void read_handler(const boost::system::error_code& error, std::size_t bytes_tran
 	std::cout << "Получено: \"" << str << "\"" << std::endl;
 	
 	uint32_t fakeResult = 3;
-	sock->async_write_some(buffer(&fakeResult, sizeof(fakeResult)), handler_write);
+	gSock->async_write_some(buffer(&fakeResult, sizeof(fakeResult)), handler_write);
 }
-
-void start_accept(socket_ptr sock);
 
 void handle_accept(socket_ptr sock, const boost::system::error_code & err) {
 	if (err) return;
 	syslog(LOG_INFO, "Принято соединение");
 	
+	gSock = sock;
 	sock->async_read_some(basio::buffer(data), read_handler);
-//	socket_ptr sock(new ip::tcp::socket(service));
 }
 
+class server {
+public:
+	server(boost::asio::io_service& io_service,
+		const ip::tcp::endpoint& endpoint)
+	: m_acceptor(io_service) {
+		m_acceptor.open(endpoint.protocol());
+		m_acceptor.set_option(ip::tcp::acceptor::reuse_address(true));
+		m_acceptor.bind(endpoint);
+		m_acceptor.listen();
+		
+		m_pSock = std::make_shared<ip::tcp::socket>(io_service);
+		
+		accept();
+	}
 
+private:
+	void accept() {
+		m_acceptor.async_accept(*m_pSock,
+			[this](boost::system::error_code err) {
+				if (!err) {
+					//std::make_shared<chat_session>(std::move(socket_), room_)->start();
+					handle_accept(m_pSock, err);
+				}
 
-void start_accept(socket_ptr sock) {
-	using namespace std::placeholders;
-	gAcc->async_accept(*sock, std::bind(handle_accept, sock, _1));
-}
+				accept();
+			});
+	}
+
+	ip::tcp::acceptor m_acceptor;
+	socket_ptr m_pSock;
+};
 
 /*
  * @brief точка входа в программу клиента
@@ -99,12 +121,10 @@ int main(int argc, char** argv) {
 	signal(SIGTERM, signalHandler);
 	
 	try {
-		io_service service;
+		boost::asio::io_service io_service;
 		ip::tcp::endpoint ep(ip::tcp::v4(), port); 
-		gAcc.reset(new ip::tcp::acceptor(service, ep));
-		sock.reset(new ip::tcp::socket(service));
-		start_accept(sock);
-		service.run();
+		server serv(io_service, ep);
+		io_service.run();
 	} catch (boost::system::system_error e) {
 		std::cerr << "Ошибка: " << e.code() << ": " << e.code().message() << std::endl;
 		syslog(LOG_ERR, "Ошибка: %s", e.code().message().c_str());
