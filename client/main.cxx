24ea0bf943a86d35db2e24c72a5232a082c489b7
Konstantin Scornyakov <netgeck@gmail.com>
2017-06-17 04:24:05 +0700

    client: конвертация вводимого слова в utf-8

--- client/main.cxx
+++ client/main.cxx
@@ -8,11 +8,15 @@
 #include "mainwindow.h"
 #include <QApplication>
 #include <QtCore/QTextCodec>
+#include <QTextCodec>
+
 /*
  * @brief точка входа в программу клиента
  */
 int main(int argc, char *argv[]) {
-	QTextCodec::setCodecForTr(QTextCodec::codecForName("System"));
+	QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+	QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
 	
 	QApplication a(argc, argv);
 	MainWindow w;
